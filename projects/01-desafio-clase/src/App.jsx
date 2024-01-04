da728c39fef09ac23b0417302cb283eb94e79f33
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-01-04 03:28:00 -0300

    Se crea desafio de clase 01, falta agregar datos y aplicar estilos.

---
+++ projects/01-desafio-clase/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Barra from './components/Barra'
+function App() {
+  return (
+    <>
+      <Barra/>
+      <div>hola</div>
+      <Barra/>
+    </>
+  )
+}
+
+export default App
---
+++ projects/01-desafio-clase/src/components/Barra.jsx
@@ -0,0 +1,7 @@
+import './barra.css'
+
+const Barra = () => {
+  return (<div className="barra"></div>)
+}
+
+export default Barra
