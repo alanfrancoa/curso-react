63913d0a7e50133be535cf55cb2055a5fb335ac1
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-01-05 16:41:30 -0300

    Se hace fetching y se muestra info condicional, soloqueda mostrar los datos traidos en un componente.

--- projects/02-desafio-clase/src/App.jsx
+++ projects/02-desafio-clase/src/App.jsx
@@ -1,13 +1,41 @@
-
+import { useState, useEffect } from "react"
 import './App.css'
 
 function App() {
+  const [IDBuscado, setIDBuscado] = useState()
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    if (IDBuscado) {
+      fetch(`https://jsonplaceholder.typicode.com/users/${IDBuscado}`)
+        .then((r) => r.json())
+        .then((userData) => {
+          setUser(userData);
+          console.log(userData);
+        })
+        .catch(error => console.error(error));
+    }
+  }, [IDBuscado])
 
 
   return (
     <div>
       <h1>Buscador de usuarios:</h1>
       <hr />
+      <div className="buscador">
+        <input type="text" placeholder="Ingrese ID del usuario" onInput={(e) => setIDBuscado(e.target.value)} />
+        <span>{IDBuscado ? '' : 'No se ha ingresado un ID.'}</span>
+      </div>
+      {IDBuscado !== undefined && user.id === undefined && (
+        <div>
+          <h4>Usuario no encontrado</h4>
+        </div>
+      )}
+      {user.id !== undefined && (
+        <div>
+          <h4>{`Datos encontrados sobre usuario-id: ${user.id}`}</h4>
+        </div>
+      )}
     </div>
   )
 }
