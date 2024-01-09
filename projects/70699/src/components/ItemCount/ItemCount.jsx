dd67a6824ddf79ed0eeb6adf8c5cee8b23e423b4
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-01-08 22:09:16 -0300

    Se agrega estilo a las tarjetas con Bootstrap, y se agrega contador con useState

--- projects/70699/src/components/ItemCount/ItemCount.jsx
+++ projects/70699/src/components/ItemCount/ItemCount.jsx
@@ -1,7 +1,22 @@
-
+import { useState } from "react"
 const ItemCount = () => {
+    const [counter, setCounter] = useState(0)
+
+    const restar = () =>{
+        if (counter > 0){
+            setCounter(counter - 1)
+        }
+        
+    }
+    const sumar = () =>{
+        setCounter(counter + 1)
+    }
   return (
-    <div>ItemCount</div>
+    <div>
+        <button className="btn btn-danger m-2" onClick={restar}>Restar</button>
+        <span>{counter}</span>
+        <button className="btn btn-success m-2" onClick={sumar}>Sumar</button>
+    </div>
   )
 }
 
--- projects/70699/src/components/ItemListComponent/ItemListComponent.jsx
+++ projects/70699/src/components/ItemListComponent/ItemListComponent.jsx
@@ -1,21 +1,34 @@
-
+import PropTypes from 'prop-types'
+import './ItemListComponent.css'
 const ItemListComponent = ({losSimpsons}) => {
 
   return (
-    <div>
+    <div className="tarjeta">
         {
             losSimpsons && losSimpsons.map(({id,nombre, description}, index) => (// primero todo el objeto desestruturado y el indice luego.
-                <div key={index}> 
-                    <p>ID: {id}</p>
-                    <p>Nombre: {nombre}</p>
-                    <p>Descripcion: {description}</p>
-                    <hr />
-                </div>
+               <div className="card tarjetaDos" key={index}>
+               <span>{id}</span>
+               <div className="card-body ">
+               <h5 className="card-title">Nombre: {nombre}</h5>
+                 <p className="card-text">Descripcion: {description}</p>
+               </div>
+             </div>
             ))
         }
     </div>
   )
 }
+
+ItemListComponent.propTypes = {
+    losSimpsons: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            nombre: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+}; // Verifica que sea un array de objetos. 
+
 
 /*{
             props.losSimpsons && props.losSimpsons.map((objeto, index) => (
