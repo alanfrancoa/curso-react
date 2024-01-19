4c38a18808c172c505862d3aa07654e257e6f1be
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-01-19 20:00:51 -0300

    Se agrega codigo de primer parte de clase nro 4.

--- projects/70699/src/App.jsx
+++ projects/70699/src/App.jsx
@@ -1,52 +1,14 @@
 import './App.css'
-import ItemCount from './components/ItemCount/ItemCount'
-import ItemListComponent from './components/ItemListComponent/ItemListComponent'
-import Poke from './components/Pokedex/Poke'
-function App() {
-  const losSimpsons= [
-    {
-      id: 1,
-      nombre: 'Homero',
-      description: 'Padre de la familia.'
-    },
-    {
-      id: 2,
-      nombre: 'Marge',
-      description: 'Madre de la familia.'
-    },
-    {
-      id: 3,
-      nombre: 'Bart',
-      description: 'Estudiante'
-    },
-    {
-      id: 4,
-      nombre: 'Lisa',
-      description: 'Saxofonista'
-    },
-    {
-      id: 5,
-      nombre: 'Maggie',
-      description: 'Bebé'
-    },
-    {
-      id: 6,
-      nombre: 'Ayudante de Santa',
-      description: 'Perro de carrera'
-    },
-    {
-      id: 7,
-      nombre: 'Bola de pelos',
-      description: 'Gatito'
-    } 
 
-  ]
-  console.log(losSimpsons)
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+//import Poke from './components/Pokedex/Poke'
+function App() {
+  
   return (
     <div>
-      <h1>Hola Mundo!</h1>
-      <ItemListComponent losSimpsons={losSimpsons}/>
-      <Poke />
+      
+      <ItemListContainer />
+      {/*<Poke /> */}
     </div>
 
     
--- projects/70699/src/components/Pokedex/Poke.jsx
+++ projects/70699/src/components/Pokedex/Poke.jsx
@@ -51,9 +51,9 @@ const Poke = () => {
                         nombre: data.name,
                         img: data.sprites.front_default,
                     })
+                    setId(data)
                 })
-                setId(data)
-                .catch((error)=>{
+                .catch((error) => {
                     console.log(error)
                 })
         }
@@ -62,7 +62,7 @@ const Poke = () => {
     return (
         <div className='container m-3'>
             <form onSubmit={handleSubmit}>
-                <input type="text" className='form-control' value={busqueda} onChange={handleInputChange}/>
+                <input type="text" className='form-control' value={busqueda} onChange={handleInputChange} />
             </form>
             <div className='d-flex justify-content-center'>
                 {
@@ -74,7 +74,7 @@ const Poke = () => {
                             <h3>{pokemon.nombre}</h3>
                             <h4 className='peso'> {pokemon.peso}</h4>
                             <img src={pokemon.img} alt={pokemon.nombre} />
-                            
+
                         </>
                     )
                 }
@@ -86,3 +86,94 @@ const Poke = () => {
 }
 
 export default Poke
+
+/*
+
+import PropTypes from 'prop-types'
+import './ItemListComponent.css'
+import ItemCount from '../ItemCount/ItemCount'
+
+const ItemListComponent = ({losSimpsons}) => {
+
+  return (
+    <div className="tarjeta">
+        {
+            losSimpsons && losSimpsons.map(({id,nombre, description}, index) => (// primero todo el objeto desestruturado y el indice luego.
+               <div className="card tarjetaDos" key={index}>
+               <span>{id}</span>
+               <div className="card-body ">
+               <h5 className="card-title">Nombre: {nombre}</h5>
+                 <p className="card-text">Descripcion: {description}</p>
+                 <ItemCount />
+               </div>
+             </div>
+            ))
+        }
+    </div>
+  )
+}
+
+ItemListComponent.propTypes = {
+    losSimpsons: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            nombre: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+}; // Verifica que sea un array de objetos. 
+
+
+/*{
+            props.losSimpsons && props.losSimpsons.map((objeto, index) => (
+                <div key={index}> 
+                    <p>ID: {objeto.id}</p>
+                    <p>Nombre: {objeto.nombre}</p>
+                    <p>Descripcion: {objeto.description}</p>
+                    <hr />
+                </div>
+            ))
+        } Declarado explicitamente
+        export default ItemListComponent 
+
+const losSimpsons= [
+    {
+      id: 1,
+      nombre: 'Homero',
+      description: 'Padre de la familia.'
+    },
+    {
+      id: 2,
+      nombre: 'Marge',
+      description: 'Madre de la familia.'
+    },
+    {
+      id: 3,
+      nombre: 'Bart',
+      description: 'Estudiante'
+    },
+    {
+      id: 4,
+      nombre: 'Lisa',
+      description: 'Saxofonista'
+    },
+    {
+      id: 5,
+      nombre: 'Maggie',
+      description: 'Bebé'
+    },
+    {
+      id: 6,
+      nombre: 'Ayudante de Santa',
+      description: 'Perro de carrera'
+    },
+    {
+      id: 7,
+      nombre: 'Bola de pelos',
+      description: 'Gatito'
+    } 
+
+  ]
+
+
+        */
