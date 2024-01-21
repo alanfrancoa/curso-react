d55e7447b2014f35ca5197dcf7f29c8bf3cdd3a4
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-01-21 14:33:40 -0300

    Se agrega y estila NavBar

--- projects/70699/src/App.jsx
+++ projects/70699/src/App.jsx
@@ -5,19 +5,24 @@ import {
   Routes,
   Route
 } from 'react-router-dom'
+import ItemCount from './components/ItemCount/ItemCount'
+import Poke from './components/Pokedex/Poke'
+
 function App() {
-  
+
   return (
     <div>
       <Router>
         <NavBar />
         <Routes>
-          <Route exact path='/' element={<ItemListContainer />}/>
+          <Route exact path='/' element={<ItemListContainer />} />
+          <Route exact path='/contador' element={<ItemCount/>} />
+          <Route exact path='/pokedex' element={<Poke />} />
         </Routes>
       </Router>
     </div>
 
-    
+
   )
 }
 
--- projects/70699/src/components/NavBar/NavBar.jsx
+++ projects/70699/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,13 @@
 import './NavBar.css'
+import {Link} from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div><h1>Soy el menu</h1></div>
+    <nav>
+        <Link className='link' to='/'>Home</Link>
+        <Link className='link' to='/contador'>Contador</Link>
+        <Link className='link' to='/pokedex'>Pokedex</Link>
+    </nav>
   )
 }
 
